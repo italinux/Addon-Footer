976d6124f228705560dc9f48beeabe1e272bf017
Matteo Montanari <matteo@italinux.com>
2021-07-30 12:30:46 +0700

    New Features

--- controller.php
+++ controller.php
@@ -36,7 +36,7 @@ class Controller extends Package
 {
 
     protected $pkgHandle = 'lazy_footer';
-    protected $pkgVersion = '1.1.5';
+    protected $pkgVersion = '1.1.6';
 
     protected $appVersionRequired = '5.7.4.2';
 
