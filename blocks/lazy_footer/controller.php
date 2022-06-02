216aed4ae7e44cdc6b04383d85cd82405115cd37
Matteo Montanari <matteo@italinux.com>
2022-06-02 13:16:02 +0700

    New Features

--- blocks/lazy_footer/controller.php
+++ blocks/lazy_footer/controller.php
@@ -259,7 +259,7 @@ class Controller extends BlockController
             $this->requireAsset('jst.animate.assets');
 
             // Import Animations CSS & JS Configuration
-            $this->requireAsset('jst.animate.' . self::$btHandlerId . '.conf');
+            $this->requireAsset('jst.animate.conf');
         }
 
         // Import CSS Font-Awesome
@@ -324,7 +324,6 @@ class Controller extends BlockController
         /** - - - - - - - - - - - - - - - - - - - - - - - - -
          * Register JS / CSS Animate for this Block
          */
-        $al->register('javascript', 'jt.jquery.migrate', 'blocks/' . $this->getBlockHandle() . '/jscript/min/jquery.migrate.min.js', $pf, $this->getPackageHandle());
         $al->register('javascript', 'jt.jquery.waypoints', 'blocks/' . $this->getBlockHandle() . '/jscript/min/jquery.waypoints.min.js', $pf, $this->getPackageHandle());
 
         // Register Assets Animate
@@ -332,14 +331,9 @@ class Controller extends BlockController
 
         $al->register('css', 'style.animate', 'blocks/' . $this->getBlockHandle() . '/style/animate.min.css', $ph, $this->getPackageHandle());
         $al->register('css', 'style.animate.delay', 'blocks/' . $this->getBlockHandle() . '/style/animate.delay.min.css', $ph, $this->getPackageHandle());
-        $al->register('css', 'style.animate.duration', 'blocks/' . $this->getBlockHandle() . '/style/animate.duration.min.css', $ph, $this->getPackageHandle());
 
         $al->registerGroup(
             'jst.animate.assets', array(
-                array(
-                    'javascript',
-                    'jt.jquery.migrate'
-                ),
                 array(
                     'javascript',
                     'jt.jquery.waypoints'
@@ -356,10 +350,6 @@ class Controller extends BlockController
                    'css',
                    'style.animate.delay'
                ),
-               array(
-                   'css',
-                   'style.animate.duration'
-               ),
             )
         );
 
@@ -368,7 +358,7 @@ class Controller extends BlockController
         $al->register('javascript-inline', $this->getJSelectorId() . '.animate-init',  '$("section#' . $this->getSectionId()  . '").lazyAnimate(' . $this->getSelectorBlock() . ');', $cf, $this->getPackageHandle());
 
         $al->registerGroup(
-            'jst.animate.' . self::$btHandlerId . '.conf', array(
+            'jst.animate.conf', array(
                array(
                    'javascript',
                    $this->getJSelectorId() . '.animate-conf'
@@ -791,7 +781,6 @@ class Controller extends BlockController
         $this->addFormExtraValues();
 
         // Add Assets to Window Overlay
-        $this->addLocalAssets('/css/tools/bootstrap-grid.min.css',  'css');
         $this->addLocalAssets('/css/tools/lazy-global-ui.css', 'css');
     }
 
