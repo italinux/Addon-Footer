623bf99978064a3257eb5298a61d3f6656fa4e1c
Matteo Montanari <matteo@italinux.com>
2026-05-17 14:26:59 +0700

    New Features

--- blocks/lazy_footer/templates/hide_credits/view.php
+++ blocks/lazy_footer/templates/hide_credits/view.php
@@ -1,6 +1,6 @@
 <?php
 /**
-.---------------------------------------------------------------------.
+.-----------------------------------------------------------------------.
 |  @package: Lazy Footer (a.k.a. add-on Footer)
 |  @version: Latest on Github
 |  @link:    http://italinux.com/addon-footer
@@ -8,18 +8,18 @@
 |
 |  @author: Matteo Montanari <matteo@italinux.com>
 |  @link:   https://matteo-montanari.com
-'---------------------------------------------------------------------'
-.---------------------------------------------------------------------------.
+'-----------------------------------------------------------------------'
+.-----------------------------------------------------------------------------.
 | @copyright (c) current year                                               |
-| ------------------------------------------------------------------------- |
-| @license: Concrete5.org Marketplace Commercial Add-Ons & Themes License   |
-|           https://concrete5.org/help/legal/commercial_add-on_license      |
+| --------------------------------------------------------------------------- |
+| @license: ConcreteCMS.com Marketplace Commercial Add-Ons & Themes License   |
+|           https://concretecms.com/about/legal/commercial-extension-license  |
 |           or just: file://lazy_footer/LICENSE.TXT                         |
-|                                                                           |
-| This program is distributed in the hope that it will be useful - WITHOUT  |
-| ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or     |
-| FITNESS FOR A PARTICULAR PURPOSE.                                         |
-'---------------------------------------------------------------------------'
+|                                                                             |
+| This program is distributed in the hope that it will be useful - WITHOUT    |
+| ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       |
+| FITNESS FOR A PARTICULAR PURPOSE.                                           |
+'-----------------------------------------------------------------------------'
 */
 defined('C5_EXECUTE') or die("Access Denied.");
 
